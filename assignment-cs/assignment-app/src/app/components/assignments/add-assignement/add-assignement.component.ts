041f6ab0c382620d8401d5f387825333a2168b9f
Lys Ciella NITEKA <lysciella.niteka@gmail.com>
2021-02-20 14:04:58 +0100

    Stepper form
    Formulaire de type Stepper-première ébauche

--- assignment-cs/assignment-app/src/app/components/assignments/add-assignement/add-assignement.component.ts
+++ assignment-cs/assignment-app/src/app/components/assignments/add-assignement/add-assignement.component.ts
@@ -1,44 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
-import { Assignment } from 'src/app/model/assignment.model';
 import { AssignmentsService } from 'src/app/services/assignments.service';
+import { Assignment } from 'src/app/model/assignment.model';
 
 
-
+/**
+ * @title Stepper with editable steps
+ */
 @Component({
   selector: 'app-add-assignement',
-  templateUrl: './add-assignement.component.html',
-  styleUrls: ['./add-assignement.component.css']
+  templateUrl: 'add-assignement.component.html',
+  styleUrls: ['add-assignement.component.css']
 })
+
 export class AddAssignementComponent implements OnInit {
-  // form
   nomDevoir:string;
   dateRendu:Date;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  isEditable = false;
 
-  constructor(private assignmentsService:AssignmentsService,
-              private router:Router) { }
+  constructor(private _formBuilder: FormBuilder, assignmentsService:AssignmentsService, private router:Router) {}
+ // constructor(private assignmentsService:AssignmentsService,
+//    private router:Router) { }
 
-  ngOnInit(): void {
-  }
-  onSubmit() {
+  ngOnInit() {
     console.log("onSubmit")
     const newAssignment = new Assignment();
 
-    newAssignment.titre = this.nomDevoir;
-    newAssignment.dateDeRendu = this.dateRendu;
-    newAssignment.rendu = false;
-    newAssignment.id = Math.ceil(Math.random()*100000);
-
-
-    //this.nouvelAssignment.emit(newAssignment);
-    //this.assignments.push(newAssignment);
-    this.assignmentsService.addAssignment(newAssignment)
-    .subscribe(message => {
-      console.log(message);
-      //on veut re-afficher la page d'accueil avec la liste
-      this.router.navigate(["/home"]);
-    })
+    this.firstFormGroup = this._formBuilder.group({
+      titreCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      daterenduCtrl: ['', Validators.required]
+    });
   }
-
 }
-  
